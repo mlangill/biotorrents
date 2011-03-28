b418d84d3846180530e6215ab53732a94b583217
Mike Chelen <mike@michaelchelen.net>
2011-03-28 01:29:51 -0400

    update install script

--- scripts/install.php
+++ scripts/install.php
@@ -5,19 +5,78 @@
 
 // php install.php --user=user --pass=pass --db=db --host=host
 
+// db options not required if secrets.php has already been set up
 
-$longopts  = array(
-    "user:",     // Required value
-    "pass:",     // Required value
-    "db:",     // Required value
-    "host::",    // Optional value
-);
 
-$options = getopt("",$longopts);
 
-print("User: ".$options["user"]."\n");
-print("Pass: ".$options["pass"]."\n");
 
+
+
+
+// check if secrets.php has already been created
+if file_exists("../include/secrets.php") {
+    // secrets.php exists
+    
+    print "Using existing secrets.php";
+
+    require_once("../include/secrets.php");
+
+} else {
+    // secrets.php does not exist
+
+    // cli options setup
+    $longopts  = array(
+    // all values optional
+        "user::",
+        "pass::",
+        "db::",
+        "host::",    
+    );
+    // get cli options
+    $options = getopt("",$longopts);
+
+    // required parameters for new secrets.php
+    $reqopt = array("user","pass","db");
+    // check for required options
+    foreach ($reqopt as $value) {
+        if (isset($options[$value])) {
+            print "Value missing: $value \n";
+            print "Please specify with: --$value=something \n";
+            exit;
+        }
+    }
+
+    // use localhost if host is not specified
+    if (!isset($options["host"])) {
+        $options["host"] = "localhost";
+    }
+
+    $user = $options["user"];
+    $pass = $options["pass"];
+    $db = $options["db"];
+    $host = $options["host"];
+
+    // create new secrets.php from template
+    $cmd = "cp ../include/secrets.php.sample ../include/secrets.php";
+    print "Creating new secrets.php from template";
+    $exec($cmd);
+
+
+    // write db info to secrets.php
+    $cmd = "sed -i 's/%%user%%/".$user."/g' ../include/secrets.php";
+    print "Modifying secrets.php with user: $user \n";    
+    exec($cmd);
+    
+    
+    print "Modifying secrets.php with user: $user \n";    
+    
+}
+
+
+    print("User: $user \n");
+    print("Pass: $pass \n");
+    print("DB: $db \n");
+    print("Host: $host \n");
 
 
 
