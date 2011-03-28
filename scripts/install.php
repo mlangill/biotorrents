be94c508b1f12291bd886c94973097b593c62f2e
Mike Chelen <mike@michaelchelen.net>
2011-03-28 03:51:38 -0400

    let install script create and update config.php

--- scripts/install.php
+++ scripts/install.php
@@ -3,16 +3,51 @@
 // first create database
 // then run this script
 
-// php install.php --user=user --pass=pass --db=db --host=host
+// example usage 
 
-// db options not required if secrets.php has already been set up
+// php install.php --user=mysqluser --pass=mysqlpass --db=somemysqldb --host=mysql.example.com --baseurl=example.com
+
+// tries to read existing secrets.php if found
 
 
+print "\n";
 
+
+// cli options setup
+$longopts  = array(
+// all values optional
+    "user::",
+    "pass::",
+    "db::",
+    "host::",
+    "baseurl::",        
+);
+// get cli options
+$options = getopt("",$longopts);
+
+// use localhost if host is not specified
+if (!isset($options["host"])) {
+    $options["host"] = "localhost";
+}
+
+// needed options
+// all needed options
+$optlist = array("user","pass","db","host","baseurl");
+// options needed for secret.php
+$seclist = array("user","pass","db","host");
+
+// check if the needed options exist
+foreach ($optlist as $value) {
+    if (!isset($options[$value])) {
+    // needed parameter missing from cli options
+    // alert user and exit
+        print "Value missing: $value \n";
+        print "Please specify with: --$value=something \n";
+        exit;
+    }
+}
 
 // read existing secrets.php or create new one
-
-
 // check if secrets.php has already been created
 if (file_exists("../include/secrets.php")) {
     // secrets.php exists
@@ -24,38 +59,6 @@ if (file_exists("../include/secrets.php")) {
 } else {
     // secrets.php does not exist
 
-
-    // cli options setup
-    $longopts  = array(
-    // all values optional
-        "user::",
-        "pass::",
-        "db::",
-        "host::",    
-    );
-    // get cli options
-    $options = getopt("",$longopts);
-
-    // use localhost if host is not specified
-    if (!isset($options["host"])) {
-        $options["host"] = "localhost";
-    }
-
-    // database parameters for new secrets.php
-    $dbsec = array("user","pass","db","host");
-    
-    // check if the needed parameters exist
-    foreach ($dbsec as $value) {
-        if (!isset($options[$value])) {
-        // needed parameter missing from cli options
-        // alert user and exit
-            print "Value missing: $value \n";
-            print "Please specify with: --$value=something \n";
-            exit;
-        }
-    }
-
-
     // create new secrets.php from template
     $cmd = "cp ../include/secrets.php.sample ../include/secrets.php";
     print "Creating new secrets.php from template \n";
@@ -64,14 +67,13 @@ if (file_exists("../include/secrets.php")) {
 
 
     // add parameters to secrets.php    
-    foreach ($dbsec as $value) {
+    print "Adding parameters to secrets.php\n";
+    foreach ($seclist as $value) {
         // edit secrets.php        
         $cmd = "sed -i 's/%%".$value."%%/".$options[$value]."/g' ../include/secrets.php";
-        print "Editing secrets.php with $value = ".$options[$value]." \n";    
         exec($cmd);
-        }  
+    }  
 }
-
 
 
 
@@ -84,5 +86,48 @@ if (file_exists("../include/secrets.php")) {
     print("DB: $mysql_db \n");
     print("Host: $mysql_host \n");
 
+
+
+    // set up loop for sql import    
+    $cmd = 'for f in tb.sql avps.sql countries.sql searchcloud.sql categories.sql reputationlevel.sql stylesheets.sql; do '
+    . "mysql -u $mysql_user -p$mysql_pass -h $mysql_host $mysql_db < ".'../SQL/$f'
+    . '; done';
+
+    // load sql
+    print "Loading SQL statemnts\n";
+    exec($cmd);
+
+
+
+
+
+// check if config.php has been created
+if (file_exists("../include/config.php")) {
+    // config.php exists
+    print "Existing config.php found \n";
+
+} else {
+    // config.php does not exist
+    // copy from template
+    print "Creating config.php from template \n";
+    $cmd = "cp ../include/config.php.sample ../include/config.php";
+    exec($cmd);
+   
+}
+
+// add baseurl to config.php
+    print "Adding base url to config.php\n"
+        .'$DEFAULTBASEURL = '
+        .$options["baseurl"]
+        ."\n";
+    $cmd = 'sed -i "s/%%BASEURL%%/'
+        .addcslashes($options["baseurl"],"/")
+        .'/g" ../include/config.php';
+    exec($cmd);
+    
+
+
+
+print "\nInstall complete. Please visit ".$options["baseurl"]." in your browser\n\n";
 
 ?>
