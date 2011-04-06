3d0e721c52f7f9962019d0cebac2e97db8c7a4c3
Morgan Langille <morgan.g.i.langille@gmail.com>
2011-04-06 12:44:50 -0700

    Updated RSS Feed. Closes #10.
    Added <pubDate> and <link> to each <item> sub-section.
    pubDate is preferred to using guid.
    link provides a link to the torrent page. Applications like twitterfeed didn't use the link in the guid field before but will use it from the link field.
    RSS Feed now passes w3c validation.

--- rss.php
+++ rss.php
@@ -20,7 +20,11 @@ list($res,$wherecatina,$pager) = search($_GET, $CURUSER);
                 //$s = "<item>\n<title>" . htmlspecialchars($a["name"] . " ($cat)- ".$a["lic_name"]) . "</title>\n" . "<category>$cat</category><version>".$a["version"]."</version><license>".$a["lic_name"]."</license><description>" . htmlentities($a["descr"]) . "</description>\n";
                 $s = "<item>\n<title>" . htmlspecialchars($a["name"] . " ($cat)- ".$a["lic_name"]) . "</title>\n" . "<category>$cat</category><description>" . htmlentities($a["descr"]) . "</description>\n";
 		print $s;
-                print "<guid>$BASEURL/details.php?id=$a[id]&amp;hit=1</guid>\n";
+		$link ="$BASEURL/details.php?id=$a[id]&amp;hit=1";
+		print "<link>$link</link>\n";
+		$date=date('r',$a['added']);
+		print "<pubDate>$date</pubDate>\n";
+                print "<guid>$link</guid>\n";
                 $filename = urlencode($a["filename"]);
                 print "<enclosure url=\"$BASEURL/download.php/$a[id]/$filename\" length=\"$a[size]\" type=\"application/x-bittorrent\"/>\n</item>\n";
         }
